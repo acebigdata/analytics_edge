f617a3c6db4525a55998f3783b2e9f416eb93af0
acebigdata <bigdataraw@gmail.com>
2016-06-19 19:56:38 -0400

    updates

--- unit_4_tree/4_1_question.R
+++ unit_4_tree/4_1_question.R
@@ -1,10 +1,12 @@
+# This code is exactly the same as illustrated in the class, just change the min bucket from 25 to 5 and 100
+
 #Initialization
 rm(list = ls())
 
 #Set file path properly
-path <- getwd()
-path <- paste(path,"/files", sep = "")
-setwd(path)
+# path <- getwd()
+# path <- paste(path,"/files", sep = "")
+# setwd(path)
 
 #Load the dataset
 stevens <- read.csv("stevens.csv")
@@ -15,7 +17,7 @@ summary(stevens)
 
 #Randomly split the data to training vs. test
 #Install and/or load library caTools
-install.packages("caTools")
+# install.packages("caTools")
 library(caTools)
 set.seed(3000)
 spl <- sample.split(stevens$Reverse, SplitRatio = 0.7)
@@ -27,14 +29,14 @@ str(train)
 str(test)
 
 #Install and/or load rpart packages
-install.packages("rpart")
+# install.packages("rpart")
 library(rpart)
 #Install and/or load rpart.plot packages
-install.packages("rpart.plot")
+# install.packages("rpart.plot")
 library(rpart.plot)
 
 #create a tree
-stevens_tree <- rpart(Reverse ~ Circuit + Issue + Petitioner + Respondent + LowerCourt + Unconst, data = train, method = "class", minbucket = 25)
+stevens_tree <- rpart(Reverse ~ Circuit + Issue + Petitioner + Respondent + LowerCourt + Unconst, data = train, method = "class", minbucket = 100)
 summary(stevens_tree)
 #plot the tree
 prp(stevens_tree)
@@ -46,7 +48,7 @@ conf_table <-table(test$Reverse, predict_cart)
 accuracy <- (conf_table[1,1] + conf_table[2,2])/(sum(conf_table))
 
 #Plot the ROC curve
-install.packages("ROCR")
+# install.packages("ROCR")
 library(ROCR)
 #generate tree to ROC without type = class arument
 predict_roc <- predict(stevens_tree, newdata = test)
@@ -57,20 +59,3 @@ pred <- prediction(predict_roc[,2], test$Reverse)
 pref <- performance(pred, "tpr","fpr")
 #calculate AUC value
 stevens_auc <- as.numeric(performance(pred,"auc")@y.values)
-
-#Random forest
-install.packages("randomForest")
-library(randomForest)
-#convert Reverse variable in both training and test sets to factor as randomTree can only run regression type
-train$Reverse <- as.factor(train$Reverse)
-test$Reverse <- as.factor(test$Reverse)
-stevens_forest <- randomForest(Reverse ~ Circuit + Issue + Petitioner + Respondent + LowerCourt + Unconst, data = train, nodesize = 25, ntree = 200)
-#make predictions based on new forest model
-predict_forest <- predict(stevens_forest, newdata = test)
-conf_table_forest <- table(test$Reverse, predict_forest)
-accuracy_forest <- (conf_table_forest[1,1] + conf_table_forest[2,2])/(sum(conf_table_forest))
-
-###Cross Validation
-#install and new package caret
-install.packages("caret")
-library(caret)
